fc0a2899ef06c77422821f12acb1e350ca76b901
yajatjotwani <yjotwani@tamu.edu>
2022-09-12 19:41:15 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -5,7 +5,7 @@ Created on Mon Sep 12 19:10:27 2022
 
 @author: 
 """
-roverInfo = 
-def get_mass(rover):
+roverInfo = {'r' : 0.3,'w_m' : 1.0,'T_s':170,'T_NL' : 0,'w_NL' : 3.80,'M_m' : 5.0,'SP_m' : 75,'RTG_m' : 90,'C_m' : 659,'d1' : 0.04,'d2' : 0.07,'Sr_m' : 1.5,'g_m' : 3.72}
+#def get_mass(rover):
     
     
