579e9785e1ef879ea0874369bc4499228334f905
Udit Parikh <uditparikh28@gmail.com>
2022-09-19 23:39:11 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 12 19:10:27 2022
-
-@author: 
+Last Edited: 09/19/2022
+@authors: Udit Parikh, Jay Jotwani
 """
-roverInfo = {'r' : 0.3,'w_m' : 1.0,'T_s':170,'T_NL' : 0,'w_NL' : 3.80,'M_m' : 5.0,'SP_m' : 75,'RTG_m' : 90,'C_m' : 659,'d1' : 0.04,'d2' : 0.07,'Sr_m' : 1.5,'g_m' : 3.72}
-#def get_mass(rover):
+rover = {
+    'wheel_assembly' : {
+        'wheel' : {
+            'radius' : 0.30, #m
+            'mass' : 1.0 #kg
+            },
+        'speed_reducer' : {
+            'type' : "reverted", #string
+            'diam_pinion' : 0.04, #m
+            'diam_gear' : 0.07, #m
+            'mass' : 1.5 #kg
+            },
+        'motor' : {
+            'torque_stall' : 170, #Nm
+            'torque_noload' : 0, #Nm
+            'speed_noload' : 3.80, #rad/s
+            'mass' : 5.0 #kg
+            },
+        },
+    'chassis' : {
+        'mass' : 659 #kg
+        },
+    'science_payload' : {
+        'mass' : 75 #kg
+        },
+    'power_subsys' : {
+        'mass' : 90 #kg
+        }
+    }
+
+planet = {
+    'g' : 3.72 #m/s^2
+    }
+
+def get_mass(rover):
+    if type(rover) != dict:
+        raise Exception("Input to get_mass must be a dictionary")
+    return((6*rover['wheel_assembly']['wheel']['mass']) + rover['wheel_assembly']['speed_reducer']['mass'] 
+           + rover['wheel_assembly']['motor']['mass'] + rover['chassis']['mass'] + rover['science_payload']['mass'] + rover['power_subsys']['mass'])
+
+def get_gear_ratio(speed_reducer):
+    if type(speed_reducer) != dict:
+        raise Exception("Input to get_gear_ratio must be a dictionary")
     
     
+    
+
+
