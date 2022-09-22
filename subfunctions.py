eee6483c1a12efa16b00205ad8f0448754d5688d
Udit Parikh <uditparikh28@gmail.com>
2022-09-22 10:07:34 -0500

    change

--- subfunctions.py
+++ subfunctions.py
@@ -4,24 +4,24 @@
 Last Edited: 09/19/2022
 @authors: Udit Parikh, Yajat Jotwani
 """
-
+from define_rover import *
 # delete test later
 #print(rover['wheel_assembly']['wheel']['radius'])
 
+rover, planet = rover1()
 def get_mass(rover):
     if type(rover) != dict:
         raise Exception("Input to get_mass must be a dictionary")
     return((6*rover['wheel_assembly']['wheel']['mass']) + rover['wheel_assembly']['speed_reducer']['mass'] 
            + rover['wheel_assembly']['motor']['mass'] + rover['chassis']['mass'] + rover['science_payload']['mass'] + rover['power_subsys']['mass'])
 
+#  or speed_reducer['type'].lower() != 'reverted'
 def get_gear_ratio(speed_reducer):
-    if type(speed_reducer) != dict or speed_reducer['type'].lower != 'reverted':
+    if type(speed_reducer) != dict:
         raise Exception("Input to get_gear_ratio must be a dictionary")
     return(((rover['wheel_assembly']['speed_reducer']['diam_gear']) / (rover['wheel_assembly']['speed_reducer']['diam_pinion']))**2)
 
-# delte test later
-#print(get_gear_ratio(rover))
-    
+# print(get_gear_ratio(rover))
     
     
 def tau_dcmotor(rover):
@@ -31,7 +31,7 @@ def tau_dcmotor(rover):
     '''
     return(rover['wheel_assembly']['motor']['torque_stall'])
 
-print(tau_dcmotor(rover))
+# print(tau_dcmotor(rover))
 
     
 
