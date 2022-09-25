9f6cbe13e85c7448d8869c51cc23a0b4ee910556
yajatjotwani <yjotwani@tamu.edu>
2022-09-25 17:24:45 -0500

    new file: analysis_rolling_res...

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
 from define_rover import *
 from subfunctions import *
 from numpy import linspace, zeros, array
@@ -6,18 +10,19 @@ from matplotlib.pyplot import plot,xlabel,ylabel
 from random import uniform
 
 rover, planet = rover1()
-Crr = 0.2
-slope_array_deg = linspace(-10,35,25)
-v_max = zeros(len(slope_array_deg))
+terrain_angle = 0
+Crr_array = linspace(0.01,0.4,25)
+v_max = zeros(len(Crr_array))
 omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 x0 = uniform(0,omega_nl/2)
 x1 = uniform(omega_nl/2,omega_nl)
 
-for a in range(len(slope_array_deg)):
-    func_find_root = lambda omega: F_net(omega, float(slope_array_deg[a]), rover, planet, Crr)
+for a in range(len(Crr_array)):
+    func_find_root = lambda omega: F_net(omega, terrain_angle, rover, planet, Crr_array[a])
     root = root_scalar(func_find_root, method='secant',x0=x0,x1=x1)
     v_max[a] = root.root
     
-plot(slope_array_deg,v_max)
-xlabel('Terrain Angle [deg]')
+plot(Crr_array,v_max)
+xlabel('Coefficient Rolling Resistance (CRR)')
 ylabel('Max Rover Speed [m/s]')
+
