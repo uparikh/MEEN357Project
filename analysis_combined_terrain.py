5bccecb57e75e9ddac41d630c346b19d1eb9e8fe
Udit Parikh <uditparikh28@gmail.com>
2022-09-26 15:58:31 -0500

    update

--- analysis_combined_terrain.py
+++ analysis_combined_terrain.py
@@ -10,12 +10,12 @@ from numpy import linspace, zeros, array
 from scipy.optimize import root_scalar
 from matplotlib.pyplot import plot,xlabel,ylabel, figure,title
 from random import uniform
-from numpy import linspace, meshgrid, zeros, shape
+from numpy import linspace, meshgrid, zeros, shape, NAN
 from mpl_toolkits.mplot3d import Axes3D
 
 
 slope_array_deg = linspace(-10,35,25)
-Crr_array = linspace(0.01,0.4,25)
+Crr_array = linspace(0.0000000000000000000000000000000000000000000001,0.4,25)
 omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 x0 = uniform(0,omega_nl/2)
 x1 = uniform(omega_nl/2,omega_nl)
@@ -26,18 +26,16 @@ N = shape(CRR)[0]
 
 for i in range(N):
     for j in range(N):
-         #if (SLOPE[i,j] < 0) and (abs(CRR[i,j]) < 1e-5):
-             #raise Exception('NAN')
-         
-         #else:
-            Crr_sample = float(CRR[i,j])
-            slope_sample = float(SLOPE[i,j])
-            
-            # here you put code to find the max speed at Crr_sample and slope_sample
-            func_find_root = lambda omega: F_net(omega, SLOPE[i,j], rover, planet, CRR[i,j])
-            root = root_scalar(func_find_root, method='secant',x0=x0,x1=x1)
-    
-            VMAX[i,j] = root.root
+        Crr_sample = float(CRR[i,j])
+        slope_sample = float(SLOPE[i,j])
+        func_find_root = lambda omega: F_net(omega, SLOPE[i,j], rover, planet, CRR[i,j])
+        for d in range(len(slope_array_deg)):
+            try:
+                omega = (root_scalar(func_find_root, method='secant', x0=x0, x1=x1)).root
+            except ValueError:
+                omega = NAN
+        VMAX[i,j] = omega
+print(VMAX)
         
 figure = figure()
 ax = Axes3D(figure, auto_add_to_figure=False)
@@ -50,3 +48,16 @@ ylabel('Terrain Angle [deg]')
 ax.set_zlabel('Max Rover Speed [m/s]')
 
 title('Max Velocity vs. CRR and Terrain Slope')
+
+
+
+'''
+loop through angels in slop array
+	try: 
+		omege = rootscalar(func, (parameters for func), method = 'bisect', bracket=[0, omega no load]).root
+	except ValueError:  # no root able to be found
+		omega = np.NaN # no value, zero is a value
+	w = np.append(w, Omega) # add value to array
+
+v = r*omega/gear ratio
+'''
